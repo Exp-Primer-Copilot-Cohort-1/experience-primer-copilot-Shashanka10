7c65b2c165a4dc9bbe4a2af85461b38d1eb98cb2
Shashanka <71580178+Shashanka10@users.noreply.github.com>
2023-09-21 04:47:35 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,10 @@
 function skillsMember() {
-    console.log('I am a member of skills')
+    var skillsMember = document.getElementById("skillsMember");
+    var skills = document.getElementById("skills");
+    var skillsMemberValue = skillsMember.value;
+    if (skillsMemberValue == "1") {
+        skills.style.display = "block";
+    } else {
+        skills.style.display = "none";
+    }
 }
